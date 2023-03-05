184d6c092cc4cea2294e7f0f74a7048fcf641d54
REVISION-PC\Seth <sethetherald@duck.com>
2023-03-05 12:29:36 +0700

    Remove test data.

--- Assets/Scripts/SceneControllers/GameOverSceneHandler.cs
+++ Assets/Scripts/SceneControllers/GameOverSceneHandler.cs
@@ -7,20 +7,18 @@ public class GameOverSceneHandler : MonoBehaviour
     private TextMeshProUGUI _totalScoreText;
     private TextMeshProUGUI _survivedTimeText;
 
-    private string totalScorePrefix = "Total Score: ";
-    private string survivedTimePrefix = "Survived Time: ";
+    private readonly string _totalScorePrefix = "Total Score: ";
+    private readonly string _survivedTimePrefix = "Survived Time: ";
 
     private void Awake()
     {
         _totalScoreText = GameObject.Find("TotalScoreText").GetComponent<TextMeshProUGUI>();
         _survivedTimeText = GameObject.Find("SurvivedTimeText").GetComponent<TextMeshProUGUI>();
 
-        SetTestData();
-
         string timed = GetSurvivedTime();
 
-        _totalScoreText.text = totalScorePrefix + DataPreserve.totalScore.ToString();
-        _survivedTimeText.text = survivedTimePrefix + timed;
+        _totalScoreText.text = _totalScorePrefix + DataPreserve.totalScore.ToString();
+        _survivedTimeText.text = _survivedTimePrefix + timed;
     }
 
     private string GetSurvivedTime()
@@ -38,14 +36,5 @@ public class GameOverSceneHandler : MonoBehaviour
 
         //Load main menu scene
         SceneManager.LoadScene("SceneMainMenu");
-    }
-
-    private static void SetTestData()
-    {
-        if (DataPreserve.survivedTime == 0 && DataPreserve.totalScore == 0)
-        {
-            DataPreserve.survivedTime = 500;
-            DataPreserve.totalScore = 2500;
-        }
     }
 }
