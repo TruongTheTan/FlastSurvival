3a9a289c1086460084560da5c2edc85b90fd74a8
REVISION-PC\Seth <sethetherald@duck.com>
2023-03-06 14:16:12 +0700

    Normalized movement vector. Removed unused codes.

--- Assets/Scripts/PrefabControllers/PlayableCharacterController.cs
+++ Assets/Scripts/PrefabControllers/PlayableCharacterController.cs
@@ -3,62 +3,46 @@
 public class PlayableCharacterController : MonoBehaviour
 {
     [SerializeField]
-    private Joystick joystick;
+    private Joystick _joystick;
 
-    private float horizontalMove = 0f;
-    private float verticalMove = 0f;
-    private readonly float moveAmount = 2.5f;
+    private float _horizontalMove = 0f;
+    private float _verticalMove = 0f;
+    private readonly float _moveAmount = 2.5f;
 
     private void Awake()
     {
-        if (joystick == null)
+        if (_joystick == null)
         {
-            joystick = FindObjectOfType<Joystick>();
+            _joystick = FindObjectOfType<Joystick>();
         }
     }
+
     private void Start()
     {
 #if UNITY_ANDROID || UNITY_IOS
-        joystick.gameObject.SetActive(true);
+        _joystick.gameObject.SetActive(true);
 #else
         joystick.gameObject.SetActive(false);
 #endif
     }
+
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         CharacterMovement();
     }
 
     private void CharacterMovement()
     {
-        horizontalMove = Input.GetAxis("Horizontal");
-        verticalMove = Input.GetAxis("Vertical");
+        _horizontalMove = Input.GetAxis("Horizontal");
+        _verticalMove = Input.GetAxis("Vertical");
 
 #if UNITY_ANDROID || UNITY_IOS
-        horizontalMove = joystick.Horizontal;
-        verticalMove = joystick.Vertical;
+        _horizontalMove = _joystick.Horizontal;
+        _verticalMove = _joystick.Vertical;
 #endif
 
-        //if (horizontalMove != 0)
-        //{
-        //    moveX = horizontalMove < 0 ? -moveAmount : moveAmount;
-        //}
-
-        //float moveY = 0;
-
-        //if (verticalMove != 0)
-        //{
-        //    moveY = verticalMove < 0 ? -moveAmount : moveAmount;
-        //}
-
-        //if (moveX != 0 || moveY != 0)
-        //{
-        //    var pos = transform.position;
-        //    transform.position = new Vector3(pos.x + moveX, pos.y + moveY, pos.z);
-        //}
-
-        Vector3 movement = new Vector3(horizontalMove, verticalMove);
-        transform.Translate(movement * moveAmount * Time.deltaTime);
+        Vector3 movement = new Vector3(_horizontalMove, _verticalMove);
+        transform.Translate(_moveAmount * Time.deltaTime * movement.normalized);
     }
 }
