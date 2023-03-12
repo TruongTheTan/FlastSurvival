5522dd515e250456b77d39d5a22ea82df9d0b5a8
REVISION-PC\Seth <sethetherald@duck.com>
2023-03-12 17:57:54 +0700

    Change enemies HP based on their type.

--- Assets/Scripts/PrefabControllers/EnemyController.cs
+++ Assets/Scripts/PrefabControllers/EnemyController.cs
@@ -2,12 +2,10 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    Transform m_target;
-    [SerializeField] float speed;
-    GameObject m_gameObject;
-    Rigidbody2D Rigidbody2Dm_Rigidbody2;
-
-
+    private Transform m_target;
+    [SerializeField] private float speed;
+    private GameObject m_gameObject;
+    private Rigidbody2D Rigidbody2Dm_Rigidbody2;
 
     [SerializeField]
     private GameObject _healthBarPrefab;
@@ -24,13 +22,10 @@ public class EnemyController : MonoBehaviour
         m_gameObject = m_target.gameObject;
     }
 
-
     void Start()
     {
         InstantiateData();
-
     }
-
 
     // Update is called once per frame
     private void FixedUpdate()
@@ -38,7 +33,6 @@ public class EnemyController : MonoBehaviour
         Vector3 direction = (m_target.position - transform.position).normalized;
         gameObject.transform.Translate(direction * Time.deltaTime * speed, Space.Self);
         timer += 1;
-
     }
 
     private void OnCollisionStay2D(Collision2D collision)
@@ -49,28 +43,46 @@ public class EnemyController : MonoBehaviour
         }
     }
 
-
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
         DecreaseHPWhenGetAttackByPlayer(collision);
     }
-
 
     private void Attack()
     {
         Debug.Log("Attack Player!!");
     }
 
-
     private void InstantiateData()
     {
+        Sprite currentSprite = gameObject.GetComponent<SpriteRenderer>().sprite;
+
+        switch (currentSprite.name)
+        {
+            case "Big Daddy":
+                _maxHealth = 200f;
+                break;
+
+            case "Blitz Jok":
+                _maxHealth = 75f;
+                break;
+
+            case "Fodder Joe":
+                _maxHealth = 100f;
+                break;
+
+            case "Explosive Dave":
+                _maxHealth = 50f;
+                break;
+
+            default: break;
+        }
+
         _healthBars = GameObject.Find("HealthBars");
         GameObject healthBar = Instantiate(_healthBarPrefab, _healthBars.transform);
         _healthBarController = healthBar.GetComponent<HealthBarController>();
         _healthBarController.SetData(gameObject, _maxHealth);
     }
-
 
     private void DecreaseHPWhenGetAttackByPlayer(Collision2D collision)
     {
