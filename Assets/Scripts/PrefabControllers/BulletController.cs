5d4fc4baa8d449167e4db13b965fe0e60f7fe8a6
REVISION-PC\Seth <sethetherald@duck.com>
2023-03-13 18:30:08 +0700

    Change damage based on weapon.

--- Assets/Scripts/PrefabControllers/BulletController.cs
+++ Assets/Scripts/PrefabControllers/BulletController.cs
@@ -1,25 +1,55 @@
+using System.Threading;
 using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
+    private int _damage;
+    private Timer _bulletTimer;
+
     // Start is called before the first frame update
     void Start()
     {
+        _bulletTimer = gameObject.AddComponent<Timer>();
+
+        switch (gameObject.tag)
+        {
+            case "Pistol":
+                _damage = 20;
+                _bulletTimer.Duration = 3;
+                break;
+
+            case "AssaultRifle":
+                _damage = 40;
+                _bulletTimer.Duration = 3;
+                break;
+
+            case "Shotgun":
+                _damage = 70;
+                _bulletTimer.Duration = 1;
+                break;
+        }
+
+        _bulletTimer.Run();
+
         GetComponent<Rigidbody2D>().velocity = transform.right * 25f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!GetComponent<Renderer>().isVisible)
+        if (_bulletTimer.Finished)
+        {
             Destroy(gameObject);
+        }
     }
-
-
-
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            collision.gameObject.GetComponent<EnemyController>().Damaged(_damage);
+        }
+
         Destroy(gameObject);
     }
 }
