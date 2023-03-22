f25760758dbc32c84fda282938ce15c283ab7f72
DESKTOP-FC6EDPL\User <truongthetan1601@gmail.com>
2023-03-22 18:20:49 +0700

    add despawn time to each support item

--- Assets/Scripts/Loot.cs
+++ Assets/Scripts/Loot.cs
@@ -1,19 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu]
 public class Loot : ScriptableObject
 {
-    public Sprite lootSprite;
-    public string lootName;
-    public string lootType;
-    public int dropChange;
-    public int among;
+	public Sprite lootSprite;
+	public string lootName;
+	public string lootType;
+	public int dropChange;
+	public int among;
 
-    public Loot(string lootName, int dropChange)
-    {
-        this.lootName = lootName;
-        this.dropChange = dropChange;
-    }
+	public Loot(string lootName, int dropChange)
+	{
+		this.lootName = lootName;
+		this.dropChange = dropChange;
+	}
 }
--- Assets/Scripts/LootBag.cs
+++ Assets/Scripts/LootBag.cs
@@ -1,41 +1,56 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LootBag : MonoBehaviour
 {
-    public GameObject dropPrabs;
-    public List<Loot> loots = new List<Loot>();
+	public GameObject dropPrabs;
+	public List<Loot> loots = new List<Loot>();
 
-    Loot GetDroppedItem()
-    {
-        int randomNumber = Random.Range(1, 101);
-        List<Loot> pos  = new List<Loot>();
-        foreach(Loot  item in loots)
-        {
-            if(randomNumber <= item.dropChange)
-            {
-                pos.Add(item);
-            }
-        }
-        if(pos.Count > 0)
-        {
-            Loot dropped = pos[Random.Range(0,pos.Count)];
-            return dropped;
-        }
-        return null;
-    }
-    public void InstantiateLoot(Vector3 spawn)
-    {
-        Loot droped = GetDroppedItem();
-        if(droped != null)
-        {
-            GameObject loot = Instantiate(dropPrabs, spawn, Quaternion.identity);
-            loot.GetComponent<SpriteRenderer>().sprite = droped.lootSprite;
+	Loot GetDroppedItem()
+	{
+		int randomNumber = Random.Range(1, 101);
+		List<Loot> pos = new List<Loot>();
+		foreach (Loot item in loots)
+		{
+			if (randomNumber <= item.dropChange)
+			{
+				pos.Add(item);
+			}
+		}
+		if (pos.Count > 0)
+		{
+			Loot dropped = pos[Random.Range(0, pos.Count)];
+			return dropped;
+		}
+		return null;
+	}
+	public void InstantiateLoot(Vector3 spawn)
+	{
+		Loot droped = GetDroppedItem();
+		if (droped != null)
+		{
+			GameObject supportItem = Instantiate(dropPrabs, spawn, Quaternion.identity);
+			supportItem.GetComponent<SpriteRenderer>().sprite = droped.lootSprite;
 
-            float drop = 300f;
-            Vector2 dropDirection = new Vector2(Random.Range(-1f,1f),Random.Range(-1f,1f));
-            loot.GetComponent<Rigidbody2D>().AddForce(dropDirection * drop, ForceMode2D.Force);
-        }
-    }
+			float drop = 300f;
+
+			Vector2 dropDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+			supportItem.GetComponent<Rigidbody2D>().AddForce(dropDirection * drop, ForceMode2D.Force);
+
+			Despawn(supportItem);
+		}
+	}
+
+
+	private void Despawn(GameObject supportItem)
+	{
+		int despawnTime = 0;
+		switch (supportItem.GetComponent<SpriteRenderer>().sprite.name)
+		{
+			case "Healing": despawnTime = 5; break;
+			case "Speed Up": despawnTime = 4; break;
+			case "Invicible Time": despawnTime = 3; break;
+		}
+		Destroy(supportItem, despawnTime);
+	}
 }
